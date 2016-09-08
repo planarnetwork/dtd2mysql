2ad0e831f604677edbca994765ff67117e9a4c95
Linus Norton <linusnorton@gmail.com>
2016-09-08 21:11:02 +0100

    updated cleaning queries, made railcard_code nullable for non standard discounts

--- src/command/CleanFaresData.ts
+++ src/command/CleanFaresData.ts
@@ -5,10 +5,14 @@ import Container from "./Container";
 export default class CleanFaresData implements Command {
 
     private static QUERIES = [
-        "DELETE FROM flow WHERE flow_id not in (SELECT flow_id FROM fare);",
+        "DELETE FROM fare WHERE fare < 50;",
+        "DELETE FROM flow WHERE usage_code = 'G';",
+        "DELETE FROM flow WHERE flow_id NOT IN (SELECT flow_id FROM fare);",
         "DELETE FROM location WHERE end_date < CURDATE();",
-        "DELETE FROM non_derivable_fare WHERE end_date < CURDATE() OR composite_indicator != 'Y';",
-        "DELETE FROM non_derivable_fare_override WHERE end_date < CURDATE() OR composite_indicator != 'Y';",
+        "DELETE FROM non_derivable_fare WHERE end_date < CURDATE() OR composite_indicator != 'Y' OR adult_fare < 50 OR child_fare < 50;",
+        "UPDATE non_derivable_fare SET adult_fare = null WHERE adult_fare = 99999 OR adult_fare > 999999;",
+        "UPDATE non_derivable_fare SET child_fare = null WHERE child_fare = 99999 OR child_fare > 999999;",
+        "DELETE FROM non_derivable_fare_override WHERE end_date < CURDATE() OR composite_indicator != 'Y' OR adult_fare < 50 OR child_fare < 50;",
         "UPDATE non_derivable_fare_override SET adult_fare = null WHERE adult_fare = 99999 OR adult_fare > 999999;",
         "UPDATE non_derivable_fare_override SET child_fare = null WHERE child_fare = 99999 OR child_fare > 999999;",
         "DELETE FROM non_standard_discount WHERE end_date < CURDATE();",
