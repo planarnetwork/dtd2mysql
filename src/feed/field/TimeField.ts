1ee1a61e79d44620d169836beee6766d650f6aeb
Linus Norton <linusnorton@gmail.com>
2017-08-28 17:29:15 +0100

    Process half second field to fix #16

--- src/feed/field/TimeField.ts
+++ src/feed/field/TimeField.ts
@@ -4,14 +4,17 @@ import {Field} from "./Field";
 export class TimeField extends Field {
 
   constructor(start: number, nullable: boolean = false, nullChars: string[] = [" ", "*"]) {
-    super(start, 4, nullable, nullChars);
+    super(start, 5, nullable, nullChars);
   }
 
   /**
-   * 0745 -> 07:45
+   * 0745 -> 07:45:00
+   * 0745H -> 07:45:30
    */
   protected parse(value: string): string {
-    return `${value.substr(0, 2)}:${value.substr(2, 2)}`;
+    const minsAndSeconds = `${value.substr(0, 2)}:${value.substr(2, 2)}`;
+
+    return value.charAt(4) === "H" ? minsAndSeconds + ":30" : minsAndSeconds + ":00";
   }
 
 }
--- test/feed/field/TimeField.spec.ts
+++ test/feed/field/TimeField.spec.ts
@@ -6,7 +6,13 @@ describe("TimeField", () => {
   it("formats the time", () => {
     const field = new TimeField(0);
 
-    chai.expect(field.extract("1230")).to.equal("12:30");
+    chai.expect(field.extract("1230 ")).to.equal("12:30:00");
+  });
+
+  it("adds half seconds", () => {
+    const field = new TimeField(0);
+
+    chai.expect(field.extract("1230H")).to.equal("12:30:30");
   });
 
 });
