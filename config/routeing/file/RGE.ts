2764cd1147e469d09715a61bfe3045ed8ac67d87
Linus Norton <linusnorton@gmail.com>
2018-03-13 06:07:30 +0000

    fixed processing of easement text

--- config/routeing/file/RGE.ts
+++ config/routeing/file/RGE.ts
@@ -1,13 +1,14 @@
 import {CSVRecord} from "../../../src/feed/record/CSVRecord";
 import {SingleRecordFile} from "../../../src/feed/file/SingleRecordFile";
 import {TextField, VariableLengthText} from "../../../src/feed/field/TextField";
+import {FixedWidthRecord} from "../../../src/feed/record/FixedWidthRecord";
 
-const record = new CSVRecord(
+const record = new FixedWidthRecord(
   "easement_text",
   ["text_ref"],
   {
     "text_ref": new TextField(0, 6),
-    "easement_text": new VariableLengthText(1, 2000)
+    "easement_text": new VariableLengthText(7, 2000)
   }
 );
 
