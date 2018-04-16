333f59fa4b920c36ddc0d438d0ecd3cd657c1b7a
Linus Norton <linusnorton@gmail.com>
2018-04-16 15:42:11 +0100

    updated fares whitelist

--- config/fares/clean.ts
+++ config/fares/clean.ts
@@ -1,27 +1,26 @@
 export const acceptedRailcards = "'','YNG','DIS','DIC','FAM','HMF','NGC','NEW','SRN','2TR','GS3','JCP'";
 
 export const ticketCodeWhitelist = `
-"10A","10B","10F","10S","1AB","1AF","1AS","1BB","1BF","1BS","1CB","1CF","1CS","1DF","1DS","1EF","1ES","1FF","1FS","1GF",
-"1GS","1HF","1HS","1SO","1SS","1ST","2AF","2AS","2BF","2CC","2CF","2CS","2DC","2DF","2DS","2EC","2EF","2ES","2FB","2FC",
-"2FF","2FS","2GB","2GC","2GF","2GS","2HC","2HF","2HS","2IS","2JC","2JS","2KC","2LC","2MC","AA1","AB1","AD1","ADT","AE1",
-"AF1","AG1","AG2","AH1","AH2","AI1","AI2","AJ2","AK2","AL2","AM1","AM2","AM3","AN2","AO2","AVK","AVL","AW1","BAO","BBO",
-"BCO","BDR","BDS","BFO","BFR","BFS","BGO","BHO","BP1","BP2","BPS","BRS","BSS","BSZ","BTS","BUS","BXS","BXZ","BYS","BZS",
-"C1R","C1S","C1X","C2S","C2X","C3S","C3X","C3Y","C4B","C4S","C4X","C4Y","C5B","C5X","C5Y","C6B","C6Y","C6Z","C7B","C7S",
-"C7Z","C8B","C8Z","CA3","CA4","CAA","CAB","CAF","CAG","CAS","CAV","CBA","CBB","CDR","CDS","CME","CNF","CNP","CNT","CO5",
-"COP","CP5","CRT","DF1","DF2","DF3","DG1","DG2","DG3","DG4","DG5","DRG","DRI","ECD","EGF","EGH","EGS","EMT","ETR","F1A",
-"F1V","F2A","F2L","F2V","F3A","F3V","F4A","F4V","F5A","F6A","F7A","F7V","F8A","F9A","FAS","FBS","FCR","FCS","FDA","FDB",
-"FDC","FDD","FDR","FDS","FDT","FFL","FFR","FFS","FFX","FHR","FIS","FNW","FOR","FOS","FP1","FP2","FPP","FRX","FSA","FSB",
-"FSC","FSO","FSR","FSS","FSX","FTC","G1R","G1S","G2R","G2S","GC3","GC4","GC5","GC6","GC7","GC8","GCD","GCG","GCQ","GCR",
-"GCS","GCT","GCU","GCV","GDR","GDS","GFD","GTR","GTS","GUR","GUS","GWC","GWD","IDO","LFB","MAF","MAS","MBF","MBS","MCF",
-"MCS","MDF","MDS","MES","MET","MFF","MFS","MGF","MGS","MHS","MIA","MIB","MID","MIF","MIS","MJF","MJS","MMS","NBA","NCA",
-"NDA","NEA","NXO","NXP","OAS","OBS","OBZ","OCS","OCT","OCZ","ODS","ODT","OES","OF1","OF2","OF3","OF4","OF5","OF6","OGS",
-"OHS","OJS","OPD","OPF","OPR","OPS","OS1","OS2","OS3","OS4","OS5","OS6","OSB","OTF","PBD","PBW","PDR","PDS","S1A","S2A",
-"S3A","S4A","S5A","S6A","S7A","S8A","S9A","SAV","SBV","SCO","SCV","SDR","SDS","SFX","SHR","SOA","SOB","SOD","SOE","SOR",
-"SOS","SOT","SPG","SRR","SSR","SSS","STO","STP","SVH","SVR","SVS","SWS","TTR","TTS","UFA","UFB","UFC","UFD","UFE","UFF",
-"UFG","UFH","UFI","USA","USB","USC","USD","USE","USF","USG","USH","USI","USW","USX","USY","USZ","VA1","VA2","VA3","VA4",
-"VA5","VA6","VAS","VBR","VBS","VCS","VDS","VER","VES","W1A","W1C","W1E","W1G","W1H","W1J","W1L","W1N","W1O","W1P","W1Q",
-"W1R","W1S","W1T","W2A","W2B","W2C","W2D","W2E","W2F","W2G","W2H","W2I","W2J","W2K","W2L","W2M","W2N","W2O","W2P","W2Q",
-"W2R","W2S","W2T","WDT","WK1","WK2","WK3","WKC","WMT","WRE","WTC","XC1","XP1","XP2","XS8"
+"1AB","1AF","1AS","1BB","1BF","1BS","1CB","1CF","1CS","1DF","1DS","1EF","1ES","1FF","1FS","1GF","1GS","1HF","1HS","1SO",
+"1SS","1ST","2AF","2AS","2BF","2CC","2CF","2CS","2DC","2DF","2DS","2EC","2EF","2ES","2FB","2FC","2FF","2FS","2GB","2GC",
+"2GF","2GS","2HC","2HF","2HS","2IS","2JC","2JS","2KC","2LC","2MC","AA1","AB1","AD1","ADT","AE1","AF1","AG1","AG2","AH1",
+"AH2","AI1","AI2","AJ2","AK2","AL2","AM1","AM2","AM3","AN2","AO2","AVK","AVL","AW1","BAO","BBO","BCO","BDR","BDS","BFO",
+"BFR","BFS","BGO","BHO","BP1","BP2","BPS","BRS","BSS","BSZ","BTS","BUS","BXS","BXZ","BYS","BZS","C1R","C1S","C1X","C2S",
+"C2X","C3S","C3X","C3Y","C4B","C4S","C4X","C4Y","C5B","C5X","C5Y","C6B","C6Y","C6Z","C7B","C7S","C7Z","C8B","C8Z","CBA",
+"CBB","CDR","CDS","CME","DF1","DF2","DF3","DG1","DG2","DG3","DG4","DG5","DRG","DRI","ECD","EGF","EGH","EGS","EMT","ETR",
+"F1A","F1V","F2A","F2V","F3A","F3V","F4A","F4V","F5A","F6A","F7A","F7V","F8A","F9A","FAS","FBS","FCR","FCS","FDA","FDB",
+"FDC","FDD","FDR","FDS","FDT","FFR","FFS","FHR","FIS","FNW","FOR","FOS","FPP","FRX","FSA","FSB","FSC","FSO","FSR","FSS",
+"FSX","FTC","G1R","G1S","G2R","G2S","GC3","GC4","GC5","GC6","GC7","GC8","GCD","GCG","GCQ","GCR","GCS","GCT","GCU","GCV",
+"GDR","GDS","GFD","GTR","GTS","GUR","GUS","IDO","LFB","MAF","MAS","MBF","MBS","MCF","MCS","MDF","MDS","MES","MET","MFF",
+"MFS","MGF","MGS","MHS","MIA","MIB","MID","MIF","MIS","MJF","MJS","MMS","NBA","NCA","NDA","NEA","NXO","NXP","OAS","OBS",
+"OBZ","OCS","OCZ","ODS","ODT","OES","OF1","OF2","OF3","OF4","OF5","OF6","OGS","OHS","OJS","OPD","OPF","OPR","OPS","OS1",
+"OS2","OS3","OS4","OS5","OS6","OSB","OTF","PBD","PBW","PDR","PDS","S1A","S2A","S3A","S4A","S5A","S6A","S7A","S8A","S9A",
+"SAV","SBV","SCO","SCV","SDR","SDS","SHR","SOA","SOB","SOD","SOE","SOR","SOS","SOT","SPG","SRR","SSR","SSS","STO","STP",
+"SVH","SVR","SVS","SWS","TTR","TTS","UFA","UFB","UFC","UFD","UFE","UFF","UFG","UFH","UFI","USA","USB","USC","USD","USE",
+"USF","USG","USH","USI","USW","USX","USY","USZ","VA1","VA2","VA3","VA4","VA5","VA6","VAS","VBR","VBS","VCS","VDS","VER",
+"VES","W1A","W1C","W1E","W1G","W1H","W1J","W1L","W1N","W1O","W1P","W1Q","W1R","W1S","W1T","W2A","W2B","W2C","W2D","W2E",
+"W2F","W2G","W2H","W2I","W2J","W2K","W2L","W2M","W2N","W2O","W2P","W2Q","W2R","W2S","W2T","WDT","WK1","WK2","WK3","WKC",
+"WMT","WRE","WTC","XC1","XP1","XP2","XS8"
 `;
 
 export const restrictionTables = ["restriction_time_date", "restriction_ticket_calendar", "restriction_train_date", "restriction_header_date"];
