0ed8b20d09e2ab05c1e60853b030496c73e9f126
Linus Norton <linusnorton@gmail.com>
2018-09-12 21:39:34 +0100

    Added stop name tidy up

--- apps/transxchange2gtfs/src/gtfs/StopsFactory.ts
+++ apps/transxchange2gtfs/src/gtfs/StopsFactory.ts
@@ -1,4 +1,4 @@
-import {TransXChange} from "../transxchange/TransXChange";
+import {TransXChange, StopPoint} from "../transxchange/TransXChange";
 import autobind from "autobind-decorator";
 import {NaPTANIndex} from "../reference/NaPTAN";
 
@@ -10,13 +10,42 @@ export type GetStops = (data: TransXChange) => string;
 @autobind
 export class StopsFactory {
 
+  private static readonly STREET_BLACKLIST = ["Road", "Street", "Lane", "Avenue"];
+
   constructor(
     private readonly naptan: NaPTANIndex
   ) {}
 
   public getStops(data: TransXChange): string {
-    return "";
+    return "stop_id,stop_code,stop_name,stop_desc,stop_lat,stop_lon,stop_url,location_type,parent_station\n"
+      + data.stops.map(this.getStop).join("\n");
   }
 
+  private getStop(stop: StopPoint): string {
+    return this.naptan[stop.StopPointRef] ? this.getNaPTANStop(this.naptan[stop.StopPointRef]) : this.getFeedStop(stop);
+  }
+
+  private getNaPTANStop([atcoCode, naptanCode, name, street, indicator, locality, parent, lng, lat]: string[]): string {
+    const specificStreet = this.shouldAddStreet(name, street) ? ", " + street : "";
+    const specificLocation = indicator !== "" ? " (" + indicator.replace("->", "") + ")" : "";
+    const city = parent || locality;
+
+    return `${atcoCode},${naptanCode},"${name}${specificLocation}${specificStreet}, ${city}",${name},${lat},${lng},,,`;
+  }
+
+  private getFeedStop(stop: StopPoint): string {
+    return `${stop.StopPointRef},,"${stop.CommonName}, ${stop.LocalityQualifier}",,0.00,0.00,,,`;
+  }
+
+  /**
+   * If the name is not the same as the street name and does not contain any words like Road or Street we can safely
+   * add the street name to the name.
+   */
+  private shouldAddStreet(name: string, street: string): boolean {
+    return street.length > 1
+      && name !== street
+      && street !== "---"
+      && StopsFactory.STREET_BLACKLIST.every(i => !name.includes(i));
+  }
 }
 
---
+++ apps/transxchange2gtfs/test/gtfs/StopsFactory.spec.ts
@@ -0,0 +1,126 @@
+import * as chai from "chai";
+import {StopsFactory} from "../../src/gtfs/StopsFactory";
+
+// tslint:disable
+
+describe("StopsFactory", () => {
+  const factory = new StopsFactory({
+    "a": ["a", "naptanA", "nameA", "streetA", "NE", "townA", "cityA", "1.00", "1.00"],
+    "b": ["b", "naptanB", "nameB", "streetB", "NE", "townB", "", "1.00", "1.00"],
+    "c": ["c", "naptanC", "nameC Road", "streetC", "NE", "townC", "cityC", "1.00", "1.00"],
+    "d": ["d", "naptanD", "nameD", "streetD", "->SW", "townD", "cityD", "1.00", "1.00"],
+  });
+  it("uses naptan data if it's available", async () => {
+    const csv = factory.getStops({
+      stops: [{
+        StopPointRef: "a",
+        CommonName: "name",
+        LocalityName: "locality",
+        LocalityQualifier: "qualifier"
+      }]
+    });
+
+    const [stop_id, stop_code, stop_name, stop_desc, stop_lat, stop_lon] = splitCSV(csv.split("\n")[1]);
+    chai.expect(stop_id).to.equal("a");
+    chai.expect(stop_code).to.equal("naptanA");
+    chai.expect(stop_name).to.equal("nameA (NE), streetA, cityA");
+    chai.expect(stop_desc).to.equal("nameA");
+    chai.expect(stop_lat).to.equal("1.00");
+    chai.expect(stop_lon).to.equal("1.00");
+  });
+
+  it("uses feed data if NaPTAN location is not found", async () => {
+    const csv = factory.getStops({
+      stops: [{
+        StopPointRef: "NotInIndex",
+        CommonName: "name",
+        LocalityName: "locality",
+        LocalityQualifier: "qualifier"
+      }]
+    });
+
+    const [stop_id, stop_code, stop_name, stop_desc, stop_lat, stop_lon] = splitCSV(csv.split("\n")[1]);
+
+    chai.expect(stop_id).to.equal("NotInIndex");
+    chai.expect(stop_code).to.equal("");
+    chai.expect(stop_name).to.equal("name, qualifier");
+    chai.expect(stop_desc).to.equal("");
+    chai.expect(stop_lat).to.equal("0.00");
+    chai.expect(stop_lon).to.equal("0.00");
+  });
+
+  it("uses the town if it is city is not present", async () => {
+    const csv = factory.getStops({
+      stops: [{
+        StopPointRef: "b",
+        CommonName: "name",
+        LocalityName: "locality",
+        LocalityQualifier: "qualifier"
+      }]
+    });
+
+    const [stop_id, stop_code, stop_name] = splitCSV(csv.split("\n")[1]);
+
+    chai.expect(stop_id).to.equal("b");
+    chai.expect(stop_code).to.equal("naptanB");
+    chai.expect(stop_name).to.equal("nameB (NE), streetB, townB");
+  });
+
+  it("adds the street name if it is useful", async () => {
+    const csv = factory.getStops({
+      stops: [{
+        StopPointRef: "c",
+        CommonName: "name",
+        LocalityName: "locality",
+        LocalityQualifier: "qualifier"
+      }]
+    });
+
+    const [stop_id, stop_code, stop_name] = splitCSV(csv.split("\n")[1]);
+
+    chai.expect(stop_id).to.equal("c");
+    chai.expect(stop_code).to.equal("naptanC");
+    chai.expect(stop_name).to.equal("nameC Road (NE), cityC");
+  });
+
+  it("removes -> from the indicator", async () => {
+    const csv = factory.getStops({
+      stops: [{
+        StopPointRef: "d",
+        CommonName: "name",
+        LocalityName: "locality",
+        LocalityQualifier: "qualifier"
+      }]
+    });
+
+    const [stop_id, stop_code, stop_name] = splitCSV(csv.split("\n")[1]);
+
+    chai.expect(stop_id).to.equal("d");
+    chai.expect(stop_code).to.equal("naptanD");
+    chai.expect(stop_name).to.equal("nameD (SW), streetD, cityD");
+  });
+});
+
+function splitCSV(csv: string): string[] {
+  const row = [];
+  let i = 0;
+
+  while (i < csv.length) {
+    if (csv.charAt(i) === '"') {
+      const endIndex = csv.indexOf('"', i + 1);
+      const value = csv.substring(i + 1, endIndex);
+
+      row.push(value);
+      i = endIndex + 2;
+    }
+    else {
+      const endIndex = csv.indexOf(',', i);
+      const value = csv.substring(i, endIndex);
+
+      row.push(value);
+      i = endIndex + 1;
+    }
+  }
+
+  return row;
+}
