42a3330f0b83b3a3262a626a7a5ef32e752e360b
Michael Tsang <miklcct@gmail.com>
2026-09-02 16:29:53 +0100

    Merge pull request #153 from miklcct/stable-route-id
    Enhance route information by using brand names shown on departure boards and colours on route maps

--- libs/gtfs/src/entity/Route.ts
+++ libs/gtfs/src/entity/Route.ts
@@ -4,13 +4,13 @@ import {AgencyID} from "./Agency";
 export interface Route {
   route_id: RouteID;
   agency_id: AgencyID;
-  route_short_name: string;
-  route_long_name: string;
+  route_short_name: string | undefined;
+  route_long_name: string | undefined;
   route_type: RouteType;
-  route_text_color: string | null;
-  route_color: string | null;
-  route_url: string | null;
-  route_desc: string | null;
+  route_text_color: string | undefined;
+  route_color: string | undefined;
+  route_url: string | undefined;
+  route_desc: string | undefined;
 }
 
 export enum RouteType {
@@ -25,7 +25,7 @@ export enum RouteType {
   ReplacementBus = 714
 }
 
-export type RouteID = number;
+export type RouteID = string;
 
 /**
  * routes.txt, as it is written. Every field of Route is a column of it.
